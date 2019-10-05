f433c73e2b703586b49dcafb7437fb4b562af1aa
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-10-05 23:51:29 +0200

    css and html demos

---
+++ htm_css/demo_00/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML & CSS 00</title>
+    <link
+      rel="stylesheet"
+      href="./css/normalize.css"
+    >
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/prism.css">
+</head>
+
+<body>
+    <div>
+        <h1>HTML</h1>
+        <p>
+            HTML (HyperText Markup Language) is the most basic building block of the Web.
+            It defines the meaning and structure of web content.
+            Other technologies besides HTML are generally used to describe a web page's
+            appearance/presentation (CSS) or functionality/behavior (JavaScript).
+        </p>
+    </div>
+
+
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">link</a>
+                    <a href="">link</a>
+                    <a href="">link</a>
+                    <a href="">link</a>
+                    <a href="">link</a>
+                    <a href="">link</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <aside>
+            links and stuff
+        </aside>
+        <article>
+
+        <form method="POST" action="index.php">
+            <input type="text" name="name">
+            <!-- <input type="submit" value="SEND"> CMD SHIFT / -->
+            <!-- <input type="reset" value="RESET"> -->
+
+            <button type="button">BUTTON</button>
+            <button type="submit">SUBMIT</button>
+        </form>
+
+
+            <img
+              src="img/gallery/city.jpeg"
+              alt="city"
+              width="100%"
+            >
+            <img
+              src="https://images.pexels.com/photos/1820041/pexels-photo-1820041.jpeg"
+              alt=""
+              width="300"
+            >
+            <h1>blog header</h1>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit libero illo accusantium similique eum nisi asperiores. Nulla esse eligendi, quis cumque placeat quia recusandae obcaecati. Ut assumenda beatae nam facere.</p>
+            <h2>some title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, impedit, earum fugiat soluta qui at eveniet culpa atque velit sint laboriosam maiores excepturi repudiandae nostrum eum ea labore deleniti neque.</p>
+            <h3>another title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet itaque possimus nostrum autem voluptatibus cumque repellendus eum sit aperiam? Nemo in illo dicta, ut eos adipisci praesentium quibusdam vel eaque?</p>
+            <iframe
+            src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
+            id="inlineFrameExample"
+            title="Inline Frame Example"
+            width="300"
+            height="200"
+            >
+          </iframe>
+
+            <footer class="article-footer"></footer>
+        </article>
+    </main>
+    <footer class="app-footer">
+      <address>
+        You can contact author at <a href="http://www.somedomain.com/contact">
+        www.somedomain.com</a>.<br>
+        If you see any bugs, please <a href="mailto:webmaster@somedomain.com">
+        contact webmaster</a>.<br>
+        You may also want to visit us:<br>
+        Mozilla Foundation<br>
+        331 E Evelyn Ave<br>
+        Mountain View, CA 94041<br>
+        USA
+      </address>
+      <address>
+        You can contact author at <a href="http://www.somedomain.com/contact">
+        www.somedomain.com</a>.<br>
+        If you see any bugs, please <a href="mailto:webmaster@somedomain.com">
+        contact webmaster</a>.<br>
+        You may also want to visit us:<br>
+        Mozilla Foundation<br>
+        331 E Evelyn Ave<br>
+        Mountain View, CA 94041<br>
+        USA
+      </address>
+      <address>
+        You can contact author at <a href="http://www.somedomain.com/contact">
+        www.somedomain.com</a>.<br>
+        If you see any bugs, please <a href="mailto:webmaster@somedomain.com">
+        contact webmaster</a>.<br>
+        You may also want to visit us:<br>
+        Mozilla Foundation<br>
+        331 E Evelyn Ave<br>
+        Mountain View, CA 94041<br>
+        USA
+      </address>
+    </footer>
+
+    <code class="language-markup">
+        <head>,
+        <title>,
+        <header>,
+        <footer>,
+        <article>,
+        <section>,
+        <p>,
+        <div>,
+        <span>,
+        <img>,
+        <aside>,
+        <audio>,
+        <canvas>,
+        <datalist>,
+        <details>,
+        <embed>,
+        <nav>,
+        <output>,
+        <progress>,
+        <video>,
+    </code>
+</body>
+</html>
