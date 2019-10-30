b9ba38da56dfcd18e06c73e2a2b6e66b17c8fb8e
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-10-30 13:10:40 +0100

    testing stuff

---
+++ javascript/while_loops.js
@@ -0,0 +1,13 @@
+/*var number = 0;
+while (number <= 12) {
+  console.log("number", number);
+  number = number + 2;
+}*/
+
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log("result", result)
