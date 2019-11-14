574ba9a8849c61348257756d395003d87a0d1079
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-11-14 08:41:52 +0100

    Create strings.js

---
+++ javascript/core/strings.js
@@ -0,0 +1,12 @@
+/*
+ * Source: JavaScript the Definitive Guide. Insert Link
+ */
+
+let s = "Noroff Fagskole";
+
+let stringObject = {
+  'first_letter':  s.charAt(),
+  'last_letter': s.charAt(s.length-1),
+}
+
+console.log('stringObject', stringObject)
