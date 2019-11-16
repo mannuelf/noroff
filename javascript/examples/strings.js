34219fc67457a5811eb438dff0f0f0e1ce992cb5
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-11-16 10:52:23 +0100

    strings strings string

--- javascript/examples/strings.js
+++ javascript/examples/strings.js
@@ -5,7 +5,7 @@
  * ISBN: 978-0-596-80552-4
  */
 
-let s = "Noroff Fagskole";
+let s = 'Noroff Fagskole';
 
 let stringObject = {
   'first_letter':  s.charAt(),
@@ -13,7 +13,12 @@ let stringObject = {
   'second_third_fourth': s.substring(1,4),
   'second_third_fourth': s.slice(1,4),
   'last_3_chars': s.slice(-3),
-  'pos_first_letter': s.indexOf("1")
+  'pos_first_letter_1': s.indexOf("1"),
+  'pos_last_letter_1': s.lastIndexOf("1"),
+  'po_first_1_3': s.indexOf("1", 3),
+  'split_substring': s.split(" "),
+  'replace_all_instances': s.replace("o", ":-)"),
+  'uppercase_all': s.toUpperCase()
 }
 
 console.log('stringObject', stringObject)
