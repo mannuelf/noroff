7c2633d1655b284c6fbc5eb3982160e2779b8dbc
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-11-17 10:40:46 +0100

    hang man

---
+++ javascript/examples/hangman/script.js
@@ -0,0 +1,66 @@
+/*
+
+Pick a random word.
+
+While the word has not yet been guessed {
+  Show the play their current progress
+  Get a guess from the player
+
+  If the player wants to quit the game {
+    Quit the game.
+  }
+  Else If the guess is not a singe letter {
+    Tell the player to pick a single letter.
+  }
+  Else {
+    If the guess is in the word {
+      Update the player's progress with the guess.
+    }
+  }
+}
+
+Congratulate the player on guessing the word.
+
+*/
+
+var words = [
+  "javascript",
+  "monkey",
+  "amazing",
+  "pancake",
+  "money",
+  "sport"
+];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+while (remainingLetters > 0) {
+  // Game code
+  // Show player progress
+  alert(answerArray.join(" "))
+  // take input from player
+  var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Please enter a single letter");
+  } else {
+    // Update the game state with the guess
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        // update answerArray and remainingLetter for every correct guess.
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
+}
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
