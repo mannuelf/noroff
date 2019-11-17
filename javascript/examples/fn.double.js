e68a0bc2752ff35f8f26ea009085a461fdccfd71
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-11-17 11:08:11 +0100

    functions functions functions

--- javascript/examples/fn.double.js
+++
@@ -1,6 +0,0 @@
-var double = function(myNumber) {
-  console.log("answer:", myNumber * 2)
-  return myNumber * 2;
-}
-
-double(double(2) + double(2));
--- javascript/examples/fn.drawFace.js
+++
@@ -1,8 +0,0 @@
-var drawFace = function(howManyTimes, whatToDraw) {
-  for (var i = 0; i < howManyTimes; i++) {
-    console.log("[" + i + "]" + whatToDraw);
-  }
-}
-
-drawFace(howManyTimes, face);
-drawFace(howManyTimes, cat);
