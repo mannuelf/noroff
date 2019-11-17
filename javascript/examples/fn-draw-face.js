4a5eaba82054d8cacf33508902754663cb2cfbd3
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2019-11-17 11:08:27 +0100

    functions functions functions

---
+++ javascript/examples/fn-draw-face.js
@@ -0,0 +1,8 @@
+var drawFace = function(howManyTimes, whatToDraw) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log("[" + i + "]" + whatToDraw);
+  }
+}
+
+drawFace(howManyTimes, face);
+drawFace(howManyTimes, cat);
